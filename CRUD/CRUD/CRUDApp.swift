7dbfbdbfd2d6ee29c9d400296856fa9cdf668d68
eqemalli <enxhi.qemalli1@gmail.com>
2024-12-27 09:38:16 +0100

    Initial commit

---
+++ CRUD/CRUD/CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDApp.swift
+//  CRUD
+//
+//  Created by Enxhi Qemalli on 27.12.24.
+//
+
+import SwiftUI
+
+@main
+struct CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRUD/CRUDTests/CRUDTests.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDTests.swift
+//  CRUDTests
+//
+//  Created by Enxhi Qemalli on 27.12.24.
+//
+
+import Testing
+@testable import CRUD
+
+struct CRUDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
